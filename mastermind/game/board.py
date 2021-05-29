b588aca16a5eb36bcedcfc8138addb85ff754bca
Nick Campbell <nickcampbell1991@gmail.com>
2021-05-28 18:03:10 -0600

    Board is printable

--- mastermind/game/board.py
+++ mastermind/game/board.py
@@ -69,3 +69,24 @@ class Board:
         guess = "----"
         hint = "****"
         self._items[name] = [code, guess, hint]
+
+    def _create_hint(self, code, guess):
+        """Generates a hint based on the given code and guess.
+
+        Args:
+            self (Board): An instance of Board.
+            code (string): The code to compare with.
+            guess (string): The guess that was made.
+
+        Returns:
+            string: A hint in the form [xxxx]
+        """ 
+        hint = ""
+        for index, letter in enumerate(guess):
+            if code[index] == letter:
+                hint += "x"
+            elif letter in code:
+                hint += "o"
+            else:
+                hint += "*"
+        return hint
